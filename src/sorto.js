ec14236617d186dbbd784637df2013dcf81eccec
pratishshr <vanroshr@gmail.com>
2017-11-08 14:48:07 +0545

    Add function to sort by length

--- src/sorto.js
+++ src/sorto.js
@@ -1,12 +1,14 @@
 import { isObject } from './utils/object';
-import { ascending, descending } from './utils/comparators';
+import { ascendingSort, descendingSort, lengthSort, lengthReverseSort } from './utils/comparators';
 
-export const ASC_ORDER = 'asc';
-export const DESC_ORDER = 'desc';
+export const SORT_ASC = 'asc';
+export const SORT_DESC = 'desc';
+export const SORT_LENGTH = 'length';
+export const SORT_LENGTH_REVERSE = 'length_reverse';
 
 /**
  * Returns appropriate comparator as per the sort string.
- * If a function is supplied, it returns that function itself.
+ * If a function is supplied, it returns the function itself.
  * 
  * @param {String|Function} sortOrder 
  * @returns {Function}
@@ -17,35 +19,64 @@ function getComparator(sortOrder) {
   }
 
   const comparators = {
-    [ASC_ORDER]: ascending,
-    [DESC_ORDER]: descending
+    [SORT_ASC]: ascendingSort,
+    [SORT_DESC]: descendingSort,
+    [SORT_LENGTH]: lengthSort,
+    [SORT_LENGTH_REVERSE]: lengthReverseSort
   };
 
   return comparators[sortOrder];
 }
 
 /**
- * Sorts object keys as per the specified order.
+ * Deep sort object keys as per the given comparator
  * 
  * @param {Object} data 
- * @param {String|Function} = 'asc' sortOrder 
+ * @param {Function} comparator 
  */
-export function sort(data, sortOrder = ASC_ORDER) {
-  if (!isObject(data)) {
-    throw new Error('Supplied data is not a valid Object');
-  }
-
+function sortObject(data, comparator) {
   let sortedData = {};
-  let comparator = getComparator(sortOrder);
   let sortedKeys = Object.keys(data).sort(comparator);
 
   sortedKeys.forEach(key => {
     if (isObject(data[key])) {
-      sortedData[key] = sort(data[key], sortOrder);
+      sortedData[key] = sortObject(data[key], comparator);
     } else {
       sortedData[key] = data[key];
     }
   });
 
   return sortedData;
+}
+
+/**
+ * Sort an array as per the given comparator
+ * 
+ * @param {Array} data 
+ * @param {Function} comparator 
+ */
+function sortArray(data, comparator) {
+  let sortedData = {};
+
+  return data.sort(comparator);
+}
+
+/**
+ * Sort data as per the specified order.
+ * 
+ * @param {Object|Array} data 
+ * @param {String|Function} = 'asc' sortOrder 
+ */
+export function sort(data, sortOrder = SORT_ASC) {
+  const comparator = getComparator(sortOrder);
+
+  if (isObject(data)) {
+    return sortObject(data, comparator);
+  }
+
+  if (Array.isArray(data)) {
+    return sortArray(data, comparator);
+  }
+
+  throw new Error('Supplied data is not a valid Object');
 }
